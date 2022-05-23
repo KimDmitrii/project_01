0e6265de28486cf7a5551ca964fffbc00c47bb43
Gennadiy <renardes@gmail.com>
2022-05-23 21:36:29 +0500

    комментарии и правки к структуре суперцикла и функциям

--- field.py
+++ field.py
@@ -1,8 +1,9 @@
 # Модуль работы с полем игры
-# Copyright by Gennadiy S. aka GennDALF
+# Copyright by Kim D.
 
 FIELD = [['']*3 for _ in range(3)]
 
+# описать функцию: что она делает?
 def field():
     global FIELD
     pass
--- players.py
+++ players.py
@@ -1,14 +1,16 @@
 # Модуль с данными игроков Крестики-Нолики
-# Copyright by Gennadiy S. aka GennDALF
+# Copyright by Kim S.
+
 from configparser import ConfigParser
 
 PLAYERS = {}
 # PLAYERS = {'Ivan': [1,1,0]}
-
 PLAYER = tuple()
+
 SAVES = {}
 # SAVES = {('ivan', 'ai1'): [[]],
 #           ('ivan', 'oleg'): [[]]}
+
 
 # Чтение data.ini
 def read_ini():
@@ -37,16 +39,20 @@ def save():
     with open('data.ini', 'w', encoding='utf-8') as config_file:
         config.write(config_file)
 
- # Выборо уровня сложности
+# Выбор уровня сложности
 def difficult_lvl():
+    # может, лучше спросить про уровень сложности: лёгкий/трудный?
     bot = input('Выберите бота(ai1, ai2): ')
     return bot
 
 # Кто ходит первым
-def firstMove():
+def first_move():
     # Здесь будет логика выбора первого хода
     # далее использовать эту функцию в функции player_name() в последнем блоке else(строка 62),
     # только я не знаю как
+    #    либо спросить пользователя, каким символом он играть хочет
+    #    либо "подкинуть монетку"
+    pass
 
 # запись имен в PLAYER
 def player_name(bot_mode=''):
@@ -63,6 +69,6 @@ def player_name(bot_mode=''):
             # добавить имя второго игрока человека
             PLAYER = (PLAYER[0], input().lower())
     else:
-
-
-
+        # здесь должна быть смена местами имён игроков в переменной PLAYER
+        # в зависимости от порядка хода (крестик первый)
+        pass
