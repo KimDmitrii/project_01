34e23ab2ff07d20689ceb78a60f2e67fa11a3663
DmitriiKim <dmitkim89@gmail.com>
2022-05-25 20:55:48 +0300

    переделан цикл в main создана функция mode, убраны функции выбора режима, выбора сложности, выбора символа в players переделан help

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Copyright by Kim D.
 
 from players import *
-from help import show_help, show_message
+from help import show_help, show_message, MESSAGES, COMMANDS
 
 # приветствие
 show_message('КРЕСТИКИ-НОЛИКИ')
@@ -13,60 +13,36 @@ if read_ini():
 
 # запуск суперцикла
 while True:
-    print('Введите команду:\nВыход из игры - "Выход" \nНовая игра - "Да"\n'
-          'Показать статистику - "stat"')
-    command = input('_> ').lower()
 
-    if command in ('quit', 'выход'):
+    command = input(MESSAGES[0]).lower()
+
+    if command in COMMANDS['quit']:
         # обработка завершения работы приложения
         break
-    elif command in ('new', 'yes', 'новая', 'да'):
+
+    elif command in COMMANDS['help']:
+        show_help()
+
+    elif command in COMMANDS['scores']:
+        show_stat()
+
+    elif command in COMMANDS['new']:
         # есть ли текущий игрок
         if not PLAYER:
             # Вводим имя первого игрока
             player_name()
+        if mode():
+            # продолжаем сохр партию
+            # после завершения отобразить статистику
+            show_stat(current=True)
+            pass
         else:
-            # выбор режима
-            game_mode = mode()
-            if game_mode in ('1', 'с ботом', 'бот'):
-
-                # загружаем игру
-                load_game()
-
-                # выбор уровня сложности
-                player_name(bot_mode=difficult_lvl())
-
-                # функция выбора символа
-                first_move()
-
-                # старт партии
-
-            elif game_mode in ('2', 'два игрока', 'двое'):
-
-                # Вводим имя второго игрока
-                # здесь не нужно передавать аргумент – это значение по умолчанию
-                player_name()
-
-                # предложение загрузить игру
-                load_game()
-
-                # функция выбора символа
-                first_move()
-
-                # старт партии
-
-    elif command == 'stat':
-        show_stat()
+            pass
+            # начинаем новую
+            # после завершения отобразить статистику
+            show_stat(current=True)
 
 
-# в функции show_stat наверное надо делать ветвление if-else на фильтр по количеству побед
-#   скорее по количеству выводимых за раз игроков, но вряд ли
 
-# для вывода статистики после партии нужно делать отдельную функцию?
-#   можно обойтись одной, но у функции тогда должен быть параметр,
-#   флаг: если его нет – вывести всю таблицу, если есть – одного или двоих
 
-# тк в этой происходит очистка переменной PLAYER, что возможно неправильно
-#   совершенно точно не должно быть очистки – не понимаю, зачем вам она нужна
 
-# docstring убрал
--- players.py
+++ players.py
@@ -1,9 +1,12 @@
 # Модуль с данными игроков Крестики-Нолики
 # Copyright by Kim S.
 
+from field import *
 from configparser import ConfigParser
+from help import MESSAGES, ANSWERS, show_message
 
-PLAYERS = {}
+
+SCORES = {}
 # PLAYERS = {'Ivan': [1,1,0], 'DIMA': [1,2,3], 'TANYA': [3,2,1]}
 PLAYER = tuple()
 SAVES = {}
@@ -13,24 +16,25 @@ SAVES = {}
 
 # Чтение data.ini
 def read_ini():
-    global PLAYERS, SAVES
+    global SCORES, SAVES
     config = ConfigParser()
     if config.read('data.ini', encoding='utf-8'):
-        PLAYERS = {name: [int(n) for n in score.split(',')]
-                   for name, score in config['Scores'].items()}
+        SCORES = {name: [int(n) for n in score.split(',')]
+                  for name, score in config['Scores'].items()}
         SAVES = {tuple(name.split(';')):
                      [[' ' if c == '-' else c for c in field[i:i+3]]
                       for i in range(0,9,3)]
                  for name, field in config['Saves'].items()}
+        # первый запуск приложения
         return True if config['General']['first'] == 'yes' else False
     else:
         raise FileExistsError
 
 # Запись в Data.ini
-def save():
+def save_ini():
     config = ConfigParser()
     config['Scores'] = {name: ','.join(str(n) for n in score)
-                        for name, score in PLAYERS.items()}
+                        for name, score in SCORES.items()}
     config['Saves'] = {';'.join(name):
                            ''.join(['-' if c == ' ' else c for r in field for c in r])
                        for name, field in SAVES.items()}
@@ -39,39 +43,12 @@ def save():
         config.write(config_file)
 
 
-# Выбор уровня сложности
-def difficult_lvl():
-    # не нужна переменная bot_mode
-    bot = input('Выберите уровень сложности\n1 - легкий\n2 - трудный\n').lower()
-    # лучше учесть несколько вариантов ответа
-    if bot in ('1', 'легкий', 'л'):
-        return 'ai1'
-    elif bot in ('2', 'трудный', 'т'):
-        return 'ai2'
-    else:
-        # только вывод сообщения и всё?
-        print('Вы ввели неверную команду')
-
-
-# Кто ходит первым
-def first_move():
-    global PLAYER
-    move = input(f"Каким символом желает играть игрок {PLAYER[0]}\n(X (ходит первым) или O)?\n").lower()
-    if move == 'x':
-        pass
-    elif move == 'o':
-        PLAYER = (PLAYER[1], PLAYER[0])
-    else:
-        # только вывод сообщения и всё?
-        print('Вы ввели неверный символ')
-
-
 # запись имен в PLAYER
-def player_name(bot_mode=''):
+def player_name(bot_mode='', *, change_order=False):
     global PLAYER
     # если имя игрока еще не вводилось
     if len(PLAYER) == 0:
-        PLAYER = (input('Введите имя игрока ').lower(), )
+        PLAYER = (input(MESSAGES[1]).lower(), )
     # если имя игрока уже вводилось
     elif len(PLAYER) == 1:
         if bot_mode:
@@ -79,46 +56,67 @@ def player_name(bot_mode=''):
             PLAYER = (PLAYER[0], bot_mode)
         else:
             # добавить имя второго игрока человека
-            PLAYER = (PLAYER[0], input('Введите имя второго игрока ').lower())
+            PLAYER = (PLAYER[0], input(MESSAGES[2]).lower())
+    if change_order:
+        PLAYER = (PLAYER[1], PLAYER[0])
+
+
 
 
 # выбор режима игры
 def mode():
-    # зачем нужна переменная, которая используется один раз?
-    return input('Выберите режим игры:\n1 - с ботом\n2 - двое игроков\n')
-
-
-# загрузка сохраненной игры
-def load_game():
-    global PLAYER, SAVES
-    if PLAYER in SAVES:
-        # после тире будет выведен список с вложенными списками – это то, чего вы хотите?
-        print(f"Вам доступна сохраненная игра \n{PLAYER} - {SAVES[PLAYER]}")
-        choice = input('Загрузить игру? ').lower()
-        # лучше учесть несколько вариантов для положительного ответа
-        if choice in ('yes', 'y', 'да', 'д'):
-            # Загрузка игры
-            pass
-        # а для всех прочих в данном случае можно реализовать вариант "нет"
+    global PLAYER
+    # запрос режима игры
+    while True:
+        gm = input(MESSAGES[3]).lower()
+        if gm in ANSWERS[3]:
+            break
+    # если одиночная
+    if gm in ANSWERS[3][:3]:
+        # есть ли сохранение для одиночной игры
+        if save := check_saves():
+            # восстановление уровня сложности и очередности из SAVES
+            PLAYER = save
+            return True
+        # запрос уровня сложности
+        while True:
+            dl = input(MESSAGES[4]).lower()
+            if dl in ANSWERS[4]:
+                break
+        # запись имени бота
+        if dl in ANSWERS[4][:3]:
+            dl = 'ai1'
         else:
-            pass
+            dl = 'ai2'
+        player_name(dl)
+    # если парная
+    else:
+        player_name()
+        if save := check_saves(single=False):
+            # восстановление уровня сложности и очередности из SAVES
+            PLAYER = save
+            return True
+
+    # выбор очередности хода
+    if not (input(MESSAGES[5]).lower() in ANSWERS[5]):
+        player_name(change_order=True)
 
 
 # вывод статистики текущего(-их) игрока(-ов)
 def show_stat(current=True):
-    global PLAYER, PLAYERS
+    global PLAYER, SCORES
     # имя текущего игрока должно заводиться в PLAYERS сразу после ввода
     # а до ввода она просто не должна вызываться
     # это вариант отработки сразу после окончания партии
     if current:
         for p in PLAYER:
             if not p.startswith('ai'):
-                print(f"Статистика побед, поражений и ничьих для - {p} - {PLAYERS[p]}")
+                print(f"Статистика побед, поражений и ничьих для - {p} - {SCORES[p]}")
     # это вариант для общей таблицы: первые десять результатов
     else:
         show_message('ТАБЛИЦА РЕЗУЛЬТАТОВ')
         print('\t\tпобед\tпоражений\tничьих')
-        for p in sorted(PLAYERS.items(),
+        for p in sorted(SCORES.items(),
                         key=lambda pair: pair[1][0],
                         reverse=True)[:10]:
             print(f'{p[0]}\t{p[1][0]}\t{p[1][1]}\t{p[1][2]}')
